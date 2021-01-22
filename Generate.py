Add the bulk, correct mistake in the computation of number of particle for the fibers

Generate.py
@@ -1,28 +1,39 @@
-#!/usr/bin/python3
 import numpy as np
 import Conversion as Conv
 from Numeric_Hex_Energy import *
 from Numeric_Fiber_Energy import *
+from Numeric_Bulk_Energy import *
 
 class Generate:
     def __init__(self,L,EPS,PTYPE):
         self.L = L
         self.EPS = EPS
         self.PTYPE = PTYPE
-    def GetBestAggregate(self,Nu,Gamma,bd,bf):
+    def GetBestAggregate(self,Nu,Gamma,bd,bf,bb):
         P = Conv.AnalyticToSimul(nu = Nu,Gamma=Gamma,l=self.L,epsilon=self.EPS,writting=False,ParticleType=self.PTYPE)
-        w,Ef= bf.Get_Best_Fiber(P)
+        w,Ef = bf.Get_Best_Fiber(P)
         n,Ed = bd.Get_Best_Disk(P)
-        if Ef<P.FB and Ef<Ed :
-            return w
-        elif Ed<P.FB:
-            return -n
+        Order,Eb = bb.Get_Best_Bulk(P)
+        # print('Nu = '+str(Nu))
+        # print('Gamma = '+str(Gamma))
+        # print('Best Fiber energy/width ='+str(Ef)+' '+str(w))
+        # print('Best Disk energy/N = '+str(Ed)+' '+str(n))
+        # print('Best Lacunar Bulk energy/order ='+str(Eb)+' '+str(Order))
+        # print('Bulk free energy = '+str(P.FB))
+        # print('Lacunar order 0 = '+str(P.Flacune))
+
+        if Ef<Eb and Ef<Ed and Ef<P.FB :
+            return np.array([0,w,0])
+        elif Ed<Eb and Ed < P.FB:
+            return np.array([n,0,0])
+        elif Eb < P.FB:
+            return np.array([0,0,Order+2])
         else :
-            return 0
-    def MakePhaseDiagram(self,Numin,Numax,NpointsNu,Gammamin,Gammamax,NpointsGamma,Nmax,WidthMax):
+            return np.array([0,0,1])
+    def MakePhaseDiagram(self,Numin,Numax,NpointsNu,Gammamin,Gammamax,NpointsGamma,Nmax,WidthMax,OrderMax):
         Nu,Gamma = np.linspace(Numin,Numax,NpointsNu,dtype=float), np.linspace(Gammamin,Gammamax,NpointsGamma,dtype=float)
         Gamma,Nu = np.meshgrid(Gamma,Nu)
-        Color = np.array([np.zeros(Nu.shape[1],dtype=float) for _ in range(Nu.shape[0])])
+        Color = np.array([np.array([np.zeros(3,dtype=float) for _ in range(Nu.shape[1])]) for _ in range(Nu.shape[0])])
         for i in range(Nu.shape[0]):
             P = Conv.AnalyticToSimul(nu = Nu[i,0],
                                     Gamma=Gamma[i,0],
@@ -32,8 +43,9 @@ class Generate:
                                     ParticleType=self.PTYPE)
             bd = BD(Nmax,P)
             bf = BF(WidthMax,P)
+            bb = BB(OrderMax,Nmax,P)
             print(Nu[i,0])
             for j in range(Nu.shape[1]):
                 #nu[i,j] = cte for i
-                Color[i,j] = self.GetBestAggregate(Nu[i,j],Gamma[i,j],bd,bf)
+                Color[i,j] = self.GetBestAggregate(Nu[i,j],Gamma[i,j],bd,bf,bb)
         return Gamma,Nu,Color

Numeric_Fiber_Energy.py
@@ -10,7 +10,8 @@ class BF:
     def __init__(self,WidthMax,P):
         #Make an array of system for each W
         self.Wmax = WidthMax
-        self.Aspect = np.array([1./3.,1./5.,0.1])
+        #self.Aspect = np.array([1./3.,1./5.,0.1])
+        self.Aspect = np.array([0.1, 0.05 , 0.01])
         self.Systems = np.array([np.empty(3,dtype=object)#np.array([
         #Sys.System(
         #Sh.Fiber(w,int(w/A),ParticleType=P.ParticleType),
@@ -20,6 +21,14 @@ class BF:
         #ParticleType=P.ParticleType)
         #for A in self.Aspect])
         for w in range(0,WidthMax,1)])
+    def Np(self,W,L,P):
+        if P.ParticleType=='Hexagon':
+            if W ==1:
+                return L
+            else :
+                return (W-0.5)*L
+        else :
+            return W*L
     def CheckInfFiber(self,w,P):
         E=list()
         if w >= self.Wmax:
@@ -37,14 +46,14 @@ class BF:
             #loop over several aspect ratio
             FiberArray = Sh.Fiber(w,int(w/A),ParticleType=P.ParticleType)
             SurfaceEnergy = Sh.SurfaceEnergy(FiberArray,J=P.J,ParticleType=P.ParticleType)
-            E.append((S.Energy+SurfaceEnergy)/(w*int(w/A)))
+            E.append((S.Energy+SurfaceEnergy)/self.Np(w,int(w/A),P))#(w*int(w/A)))
 
         popt,pconv = curve_fit(line,self.Aspect,E)
         print('a, b = '+str(popt))
         fig,ax = plt.subplots()
         ax.plot(self.Aspect,line(self.Aspect,popt[0],popt[1]))
         ax.scatter(self.Aspect,E)
-        return fig, ax  
+        return fig, ax
     def Get_Einf(self,w,P):
         E=list()
         if w >= self.Wmax:
@@ -62,7 +71,7 @@ class BF:
             #loop over several aspect ratio
             FiberArray = Sh.Fiber(w,int(w/A),ParticleType=P.ParticleType)
             SurfaceEnergy = Sh.SurfaceEnergy(FiberArray,J=P.J,ParticleType=P.ParticleType)
-            E.append((S.Energy+SurfaceEnergy)/(w*int(w/A)))
+            E.append((S.Energy+SurfaceEnergy)/self.Np(w,int(w/A),P))#(w*int(w/A)))
         try :
             popt,pconv = curve_fit(line,self.Aspect,E)
         except ValueError:

Run.py
@@ -11,17 +11,18 @@ NAME = 'PD_Hex_L_5'
 numin = 0.34
 numax = 0.99
 Gammamin = 0.
-Gammamax = 1.25
-NpointsNu = 5
-NpointsGamma=10
+Gammamax = 0.5
+NpointsNu = 10
+NpointsGamma=100
 Nmax = 500
-Wmax = 10
+Wmax = 30
+OrderMax = 0
 
-L = 5.
+L = 7.5
 PTYPE = 'Hexagon'
 EPS = 0.01
 G = Generate(L,EPS,PTYPE)
-Gamma,nu,Color = G.MakePhaseDiagram(numin,numax,NpointsNu,Gammamin,Gammamax,NpointsGamma,Nmax,Wmax)
+Gamma,nu,Color = G.MakePhaseDiagram(numin,numax,NpointsNu,Gammamin,Gammamax,NpointsGamma,Nmax,Wmax,OrderMax)
 #Gamma,nu,Color = np.loadtxt('Gamma.txt',dtype=float),np.loadtxt('nu.txt',dtype=float),np.loadtxt('Color.txt',dtype=float)
 #np.savetxt('Gamma.txt',Gamma)
 #np.savetxt('nu.txt',nu)
@@ -30,19 +31,35 @@ Gamma,nu,Color = G.MakePhaseDiagram(numin,numax,NpointsNu,Gammamin,Gammamax,Npoi
 
 # Create the figure, and save it
 
-BulkColor = (86./255,42./255.,132./255)
+#BulkColor = (86./255,42./255.,132./255)
+cmaplist = [(86./255,42./255.,132./255.,1.), (156./255,18/255.,109/255.,1.),(204./255.,35./255.,129./255.),(0,0,0,1)]
+cmap = mcolors.LinearSegmentedColormap.from_list(
+    'Custom cmap', cmaplist, 3)
 fig,ax = plt.subplots(figsize=(16,10))
-cmap = cm.Reds#.reversed()
-cmap.set_bad(color=BulkColor)
-masked=np.ma.masked_array(Color,Color>=0)
-psm = ax.pcolormesh(Gamma,nu,-masked,cmap=cm.Reds)#,norm=mcolors.LogNorm())
-psm2=ax.pcolormesh(Gamma,nu,np.ma.masked_array(Color,Color<=0),cmap=cm.Blues)
+#cmap = cm.Reds#.reversed()
+#cmap.set_bad(color=BulkColor)
+#masked=np.ma.masked_array(Color,Color>=0)
+#psm = ax.pcolormesh(Gamma,nu,-masked,cmap=cm.Reds)#,norm=mcolors.LogNorm())
+#psm2=ax.pcolormesh(Gamma,nu,np.ma.masked_array(Color,Color<=0),cmap=cm.Blues)
 
+#define the hexagon region :
+HexRegion = np.ma.masked_array(Color[:,:,0],Color[:,:,0]<=0)
+psmHex = ax.pcolormesh(Gamma,nu,HexRegion,cmap=cm.Reds)#,norm=mcolors.LogNorm())
+cba=plt.colorbar(psmHex)
+#Define the Fiber Region :
+FiberRegion = np.ma.masked_array(Color[:,:,1],Color[:,:,1]<=0)
+psmFiber = ax.pcolormesh(Gamma,nu,FiberRegion,cmap=cm.Blues)
+cbb=plt.colorbar(psmFiber)
+#Define the different bulk regions :
+BulkRegion = np.ma.masked_array(Color[:,:,2],Color[:,:,2]<=0)
+psmBulk = ax.pcolormesh(Gamma,nu,BulkRegion,cmap = cmap)
+#cbc = plt.colorbar(psmBulk)
 #psm = ax.pcolormesh(J,Ka,-masked,cmap=cmap)#,norm=colors.LogNorm())
 #psm2=ax.pcolormesh(J,Ka,np.ma.masked_array(Width,Width<=0),cmap=cm.Blues)
+#print(Color[:,:,0])
+#print(Color[:,:,1])
+#print(Color[:,:,2])
 
-cba=plt.colorbar(psm)
-cbb=plt.colorbar(psm2)
 cba.set_label('Disk Radius',fontsize=20)
 cbb.set_label('fiber width',fontsize=20)
 plt.xlabel('$\Gamma$ : rescaled surface tension',fontsize=20)

Shape.py
@@ -94,13 +94,15 @@ def Fiber(Width,Length,ParticleType='Triangle'):
     elif ParticleType=='Hexagon':
         return Fiber2(Width,Length)
 def Fiber2(W,L):
-        Res=np.array([np.zeros(L) for _ in range(W+int(L//2+0.5))])
-        for l in range(L):
-            if l%2==0:
-                Res[l//2:W+l//2,l]=1
-            else :
-                Res[l//2+1:W+l//2,l]=1
-        return np.flip(np.transpose(Res),0)
+    if W==1:
+        return Fiber4(W,L)
+    Res=np.array([np.zeros(L) for _ in range(W+int(L//2+0.5))])
+    for l in range(L):
+        if l%2==0:
+            Res[l//2:W+l//2,l]=1
+        else :
+            Res[l//2+1:W+l//2,l]=1
+    return np.flip(np.transpose(Res),0)
 def Fiber3(W,L):
         Res=np.array([np.zeros(L) for _ in range(W+int(L//2+0.5))])
         for l in range(L):
@@ -113,7 +115,29 @@ def Fiber4(W,L):
     Res=np.array([np.zeros(W) for _ in range(L)])
     Res[:]=1
     return np.transpose(Res)
-
+def D(ij,ij0,PType):
+    if PType == 'Hexagon':
+        return (((ij[0]-ij0[0])+0.5*((ij[1]-ij0[1])))**2+((ij[1]-ij0[1])*0.866)**2)**0.5
+    else :
+        return (((ij[0]-ij0[0])*0.5)**2+((ij[1]-ij0[1])*0.866)**2)**0.5
+def Disk(L,ParticleType):
+    if ParticleType == 'Hexagon':
+        Ly,Lx = L,L
+    else:
+        Ly,Lx = L//2,L
+    State = np.array([[1 for _ in range(Ly+1)] for _ in range(Lx+1)])
+    ij0 = (Lx//2,Ly//2)
+    R = Lx//4
+    for i in range(State.shape[0]):
+        for j in range(State.shape[1]):
+            if D(ij0,(i,j),ParticleType) > R:
+                State[i,j]=0
+    if ParticleType == 'Triangle':
+        for i in range(State.shape[0]):
+            for j in range(State.shape[1]):
+                if Get_Neighbors(State,(i,j),Occupied=True,ParticleType=ParticleType).__len__()== 0:
+                    State[i,j] = 0
+    return State
 def Get_Neighbors(Array,ij,Occupied=False,Free=False,Border=False,ParticleType = 'Triangle'):
         Lx, Ly = Array.shape[0], Array.shape[1]
         if ParticleType == 'Triangle':
@@ -157,3 +181,51 @@ def SurfaceEnergy(Array,J=1.,ParticleType='Triangle'):
             if Array[ij] == 1:
                 surface+=Get_Neighbors(Array,ij,Free=True,Border=True,ParticleType=ParticleType).__len__()
     return J*surface
+def GetSurfaceParticle(Array,ParticleType = 'Triangle'):
+    SurfParticle = 0
+    for i in range(Array.shape[0]):
+        for j in range(Array.shape[1]):
+            ij = (i,j)
+            if Array[ij] == 1:
+                if Get_Neighbors(Array,ij,Free=True,Border=True,ParticleType=ParticleType).__len__() != 0:
+                    SurfParticle+=1
+    return SurfParticle
+def Lacunar(Size, Order =1, ParticleType='Triangle'):
+    Array = Parallel(Size,ParticleType)
+    if ParticleType == 'Hexagon':
+        Ly,Lx = Array.shape[1]//2,Array.shape[0]//2
+    else:
+        Ly,Lx = Array.shape[1]//2,Array.shape[0]//2
+    if Lx%2 == 1:
+        Lx+=1
+    if Ly%2 ==1 :
+        Ly+=1
+    # initialize the list of the particle to remove
+    To_Delete = {(Lx,Ly)}
+    while To_Delete.__len__()!= 0:
+        # Stor the initial To_delete
+        #Old_To_Delete = To_Delete.copy()
+        New_Delete = set()
+        # add the neighboring ij's
+        for ij in To_Delete:
+            Array[ij] = 0
+            for neigh in Get_Lacune(ij,Order,ParticleType):
+                # if they follow two conditions :
+                # They are in the box
+                try :
+                    # They are not already empty
+                    if Array[neigh]==1:
+                        New_Delete.add(neigh)
+                except IndexError:
+                    pass
+        # Repete with the new added  one
+        To_Delete = New_Delete
+    return Array
+def Get_Lacune(ij,Order,ParticleType):
+    i,j=ij[0],ij[1]
+    if Order ==0 and ParticleType == 'Hexagon':
+        return  [(i-1,j+2),(i+1,j+1),(i+2,j-1),(i+1,j-2),(i-1,j-1),(i-2,j+1)]
+    if Order == 1 and ParticleType == 'Hexagon':
+        return [(j,j+2),(i+2,j),(i+2,j-2),(i,j-2),(i-2,j),(i-2,j+2)]
+    if Order == 2 and ParticleType == 'Hexagon':
+        return [(i-2,j+4),(i+2,j+2),(i+4,j-2),(i+2,j-4),(i-2,j-2),(i-4,j+2)]